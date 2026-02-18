a683f77397bc6ba3e2463dac4b4007824d26551e
keerthana0179-web <keerthana0179@gmail.com>
2026-02-18 14:52:40 +0530

    Add files via upload

---
+++ QUEUEARR.C
@@ -0,0 +1,75 @@
+#include<stdio.h>
+#include<conio.h>
+#define MAX 5
+
+int queue[MAX];
+int front=-1;
+int rear=-1;
+
+void enqueue(int x)
+{
+ if(rear==MAX-1)
+ {
+  printf("Queue overflow/n");
+ }
+ else
+ {
+  if(front==-1)
+   front=0;
+  rear++;
+  queue[rear]=x;
+  printf("%d enqueued\n",x);
+ }
+}
+void dequeue()
+{
+ if(front==-1||front>rear)
+ {
+  printf("Queue Underflow\n");
+ }
+ else
+ {
+  printf("%d dequeued\n",queue[front]);
+  front++;
+ }
+}
+void peek()
+{
+ if(front==-1||front>rear)
+ {
+  printf("Queue Underflow\n");
+ }
+ else
+ {
+  printf("Front:%d\n",queue[front]);
+ }
+}
+void display()
+{
+int i;
+ if(front==-1||front>rear)
+ {
+  printf("Queue is empty\n");
+ }
+ else
+ {
+  printf("Queue elements:");
+  for(i=front;i<=rear;i++)
+  {
+   printf("%d",queue[i]);
+  }
+  printf("\n");
+ }
+}
+void main()
+{
+ clrscr();
+ enqueue(10);
+ enqueue(20);
+ enqueue(30);
+ peek();
+ display();
+ dequeue();
+ peek();
+ getch();
+}
